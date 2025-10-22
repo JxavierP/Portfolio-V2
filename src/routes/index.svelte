4db63720688663875edae40061b68a6871b9eb3e
Jxavier <182994886+JxavierP@users.noreply.github.com>
2025-10-21 20:18:07 -0400

    init: initial setup

---
+++ src/routes/index.svelte
@@ -0,0 +1,24 @@
+<script lang="ts"></script>
+
+<main class="main">
+  <p class="hello">ELEGANT</p>
+  <p class="hello">DESIGN, ENGINEER.</p>
+</main>
+
+<style>
+  .main {
+    background-color: #1c1f24;
+    color: #e3e0db;
+    display: flex;
+    height: 100%;
+    flex-direction: column;
+
+    align-items: center;
+    justify-content: center;
+  }
+  .hello {
+    font-size: 7rem;
+    font-weight: bolder;
+    font-family: "Almarai", sans-serif;
+  }
+</style>
