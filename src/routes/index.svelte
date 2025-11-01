f2e305cd00f77b8d8245055b66f7dc49ad112bbb
Jxavier <182994886+JxavierP@users.noreply.github.com>
2025-11-01 11:25:33 -0400

    init: initial setup and design system

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,25 +1,25 @@
 <script lang="ts"></script>
 
 <main class="main">
-  <p class="hello">I INNOVATE.</p>
-  <p class="hello">DEVELOP. IMPACT</p>
-  <p class="hello">ITERATE.</p>
+	<p class="hello">INNOVATE.</p>
+	<p class="hello">DEVELOP, EXECUTE.</p>
+	<p class="hello">ITERATE.</p>
 </main>
 
 <style>
-  .main {
-    background-color: #1c1f24;
-    color: #e3e0db;
-    display: flex;
-    height: 100%;
-    flex-direction: column;
+	.main {
+		background-color: #1c1f24;
+		color: #e3e0db;
+		display: flex;
+		height: 100%;
+		flex-direction: column;
 
-    align-items: center;
-    justify-content: center;
-  }
-  .hello {
-    font-size: 7rem;
-    font-weight: bolder;
-    font-family: "Almarai", sans-serif;
-  }
+		align-items: center;
+		justify-content: center;
+	}
+	.hello {
+		font-size: 7rem;
+		font-weight: bolder;
+		font-family: "Almarai", sans-serif;
+	}
 </style>
